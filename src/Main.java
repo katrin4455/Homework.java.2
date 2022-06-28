cdf9b03aad38c2dcb3e4b67a46d1296c53fe7c4d
katrin4455 <katrin.tem2013@gmail.com>
2022-06-28 17:34:29 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println();
+
+        int cost = 200;
+
+        int perMile = 20;
+
+        int mile = cost / perMile;
+
+        System.out.println("Количесто миль: " + mile);
+    }
+}
